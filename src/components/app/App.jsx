09c8a796ed62ecefd70171eb238cdc116fa6deca
AJ.Delcimmuto <Anthony.Delcimmuto@pluto.tv>
2021-12-19 11:32:35 -0600

    first commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import '../../stylesheets/App.css';
+import ChessBoard from '../ChessBoard/ChessBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <ChessBoard/> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
